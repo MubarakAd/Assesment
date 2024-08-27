bbdc5810dcf0341736d35d1ad917bbe7d7d7b0a3
Mubarak Adem <mubarakadem@gmail.com>
2024-08-27 10:44:06 +0300

    Initial commit from Create Next App

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=" flex justify-between">
+        <div>
+
+        </div>
+        <div className="flex gap-2">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default NavBar
